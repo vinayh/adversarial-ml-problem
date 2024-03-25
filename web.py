62d8a3f4076b89a3f2127975a32233ddc944e23a
Vinay Hiremath <github@vinayh.com>
2024-03-25 20:55:32 +0000

    Added processing forward pass top preds and displaying in simple Jinja template

--- web.py
+++ web.py
@@ -1,5 +1,5 @@
 import json
-
+import torch
 from flask import Flask, request, render_template
 from PIL import Image
 
@@ -7,10 +7,22 @@ from adversarial import AdversarialGenerator
 
 
 def read_imagenet_labels(path: str) -> dict[int, [str, str]]:
+    """
+    Reads JSON of ImageNet labels into a Python dict
+    {label_index_int: [label_id, label_name]}
+    """
     with open(path) as json_data:
         data = json.load(json_data)
         return {int(k): v for k, v in data.items()}
 
+
+def topk_to_tuples(preds: torch.return_types.topk) -> [tuple[str, float]]:
+    """
+    Reformats PyTorch topk return type (values and indices) to list of tuples
+    [(label_name_str, confidence_float)]
+    """
+    return [(labels[int(preds.indices[i])], float(preds.values[i])) for i in range(len(preds.values))]
+    
 
 app = Flask(__name__)
 labels = read_imagenet_labels("imagenet_labels.json")
@@ -25,7 +37,7 @@ def index():
 def predict():
     orig_image = Image.open(request.files["image"])
     orig_preds = adversarial_generator.forward(orig_image)
-    return str(orig_preds)
+    return render_template("predict.html", orig_preds=topk_to_tuples(orig_preds))
     
 
 if __name__ == "__main__":
